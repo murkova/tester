2196bfb127037f07b6c9decb9f6a4230892042a7
murkova <totalbeatrecords@gmail.com>
2021-03-20 18:27:58 +0100

    texto modificado

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Ahora si que vagit status'))
+app.get('/', (req, res) => res.send('<h1>Express con html</h1>'))
 
 app.listen(3000);
 console.log('listen on port 3000')
