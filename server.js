d9d0256082b3586660b08123648a85a419d63439
murkova <totalbeatrecords@gmail.com>
2021-03-20 20:25:26 +0100

    pasamos al 80

--- server.js
+++ server.js
@@ -4,5 +4,5 @@ const app = express();
 
 app.get('/', (req, res) => res.send('<h1>Express con html de prueba sin 3000</h1>'))
 
-app.listen(8080);
-console.log('listen on port 8080')
+app.listen(80);
+console.log('listen on port 80')
