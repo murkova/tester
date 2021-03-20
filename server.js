c6a2d4da202dc1da40f3705ceecb97fb81b37930
murkova <totalbeatrecords@gmail.com>
2021-03-20 20:08:18 +0100

    port updated

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('<h1>Express con html de prueba</h1>'))
+app.get('/', (req, res) => res.send('<h1>Express con html de prueba sin 3000</h1>'))
 
-app.listen(3000);
-console.log('listen on port 3000')
+app.listen(8080);
+console.log('listen on port 8080')
